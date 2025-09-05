e64569a5f3ad976e2452b75ecd67dfec9643d330
alecurtu <63121923+alecurtu@users.noreply.github.com>
2025-09-04 19:31:20 -0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import CLVCalculator from '@/components/CLVCalculator';
-export default function Page() {
-  return (
-    <main className="mx-auto max-w-5xl px-4 py-8">
-      <CLVCalculator />
-    </main>
-  );
-}
